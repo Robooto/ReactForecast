4d5c71a5cf154ca21d54ad00e63469bdc4ed3fb1
Sam Russell <samrussell63@gmail.com>
2016-12-11 10:03:56 -0800

    adding google maps and sparklines grids

--- notes.md
+++ notes.md
@@ -4,11 +4,25 @@
     - Ajax requests in redux are kind of complicated to setup at first (like most things)
 - Middlewares
     - Middlewares can modify actions prior to the actions hitting the reducers
+- Arrays in react
+    - Need to use keys when using arrays
+- Props
+    - Props can be passed into components by passing them as attributes on the dom <chart color="red" />
+    - used like props.color
+- React Sparklines
+    - 3rd party component
 
 # App Components
 - SeachBar
     - Search bar needs to be a container due to it talking to redux
     - Controlled field is a valued input that is set by the component
+- WeatherList
+    - List of weather information
+    - Mapped to the weather prop and any time that changes it will rerender
 - App
     - Chart
     - ForcastList
+
+# Containers
+- Chart
+    - Chart doesn't need to be aware of any state so it can be a functional react component
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
     renderWeather(cityData) {
+        // const lon = cityData.city.coord.lon;
+        // const lat = cityData.city.coord.lat;
+        // same as above using es6 destructuring
+        const { lon, lat } = cityData.city.coord;
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
         return (
-            <tr key={cityData.city.name}>
-                <td>{cityData.city.name}</td>
+            <tr key={name}>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={pressures} color="green" units="hPa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
         );
     }
 
     render() {
         return (
-            <table className="table table-hoover">
+            <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temerature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -32,6 +46,7 @@ class WeatherList extends Component {
 
 //pulls off object property
 // creates property of same key
+// {weather} === {weather: weather}
 function mapStateToProps({weather}) {
     return { weather };
 }
